5c39a6320a9524067dacf0bb7365f950e8421922
12sreevani-hue <12sreevani@gmail.com>
2026-03-12 14:55:38 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,16 @@
-
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
-import Home from './pages/Home';
-import NodeCreation from './pages/NodeCreation';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import NodeCreation from "./pages/NodeCreation";
+import "./App.css";
 
 function App() {
-  const [sidebarOpen, setSidebarOpen] = useState(false);
-
-  const toggleSidebar = () => {
-    setSidebarOpen(!sidebarOpen);
-  };
-
   return (
     <Router>
-      <div className="App">
-
-        <main className={`main-content ${sidebarOpen ? 'sidebar-open' : ''}`}>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/node-creation" element={<NodeCreation />} />
-          </Routes>
-        </main>
-      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/node-creation" element={<NodeCreation />} />
+      </Routes>
     </Router>
   );
 }
