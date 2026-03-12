440e66935263a85106b497a8edcecbe7ec4d8db2
unknown <12sreevani@gmail.com>
2026-03-12 23:28:03 +0530

    fix App.js syntax

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,3 @@
-
 import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
@@ -26,20 +25,6 @@ function App() {
           </Routes>
         </main>
       </div>
-=======
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import NodeCreation from "./pages/NodeCreation";
-import "./App.css";
-
-function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/node-creation" element={<NodeCreation />} />
-      </Routes>
     </Router>
   );
 }
