7ea67f7e946e6c16a0980f8c44beaed2686d0786
12sreevani-hue <12sreevani@gmail.com>
2026-03-12 14:01:10 +0530

    Update Navbar.js

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,1 +1,11 @@
+import React from "react";
 
+function Navbar() {
+  return (
+    <div style={{background:"#333",color:"white",padding:"10px"}}>
+      Navbar
+    </div>
+  );
+}
+
+export default Navbar;
